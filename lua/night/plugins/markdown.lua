2034001b5014fd253559483cc3bb733184af9567
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-10-12 21:24:19 +0700

    Update markdown.lua

--- lua/night/plugins/markdown.lua
+++ lua/night/plugins/markdown.lua
@@ -1,9 +1,8 @@
 return {
-    'MeanderingProgrammer/markdown.nvim',
-    main = "render-markdown",
-    opts = {},
-    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
 }
