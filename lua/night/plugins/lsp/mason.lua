4cd11b751ea949e82f97924bcc410ed35b5da931
Artem <154594695+Nighty3098@users.noreply.github.com>
2025-11-21 14:23:12 +0700

    Update

--- lua/night/plugins/lsp/mason.lua
+++ lua/night/plugins/lsp/mason.lua
@@ -5,15 +5,10 @@ return {
         "WhoIsSethDaniel/mason-tool-installer.nvim"
     },
     config = function()
-        -- import mason
         local mason = require("mason")
-
-        -- import mason-lspconfig
-        -- local mason_lspconfig = require("mason-lspconfig")
 
         local mason_tool_installer = require("mason-tool-installer")
 
-        -- enable mason and configure icons
         mason.setup(
             {
                 ui = {
@@ -26,25 +21,13 @@ return {
             }
         )
 
-        --mason_lspconfig.setup({
-        -- list of servers for mason to install
-        --ensure_installed = {
-        --  "tsserver",
-        --  "html",
-        --  "cssls",
-        --  "tailwindcss",
-        --  "svelte",
-        --  "lua_ls",
-        --  "graphql",
-        --  "emmet_ls",
-        --  "prismals",
-        --  "pyright",
-        --},
-        --})
-
         mason_tool_installer.setup(
             {
                 ensure_installed = {
+                    "html",
+                    "pyright",
+                    "cssls",
+                    "lua_ls",
                     "prettier", -- prettier formatter
                     "stylua", -- lua formatter
                     "isort", -- python formatter
