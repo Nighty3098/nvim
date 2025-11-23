318b54893f6b108d7dd23dacda1ce003c97f0862
Artem <154594695+Nighty3098@users.noreply.github.com>
2025-11-23 16:24:25 +0700

    Update

--- lua/night/plugins/lsp/mason.lua
+++ lua/night/plugins/lsp/mason.lua
@@ -2,40 +2,34 @@ return {
     "williamboman/mason.nvim",
     dependencies = {
         "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim"
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
     config = function()
         local mason = require("mason")
 
         local mason_tool_installer = require("mason-tool-installer")
 
-        mason.setup(
-            {
-                ui = {
-                    icons = {
-                        package_installed = "✓",
-                        package_pending = "➜",
-                        package_uninstalled = "✗"
-                    }
-                }
-            }
-        )
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
 
-        mason_tool_installer.setup(
-            {
-                ensure_installed = {
-                    "html",
-                    "pyright",
-                    "cssls",
-                    "lua_ls",
-                    "prettier", -- prettier formatter
-                    "stylua", -- lua formatter
-                    "isort", -- python formatter
-                    "black", -- python formatter
-                    "pylint",
-                    "eslint_d"
-                }
-            }
-        )
-    end
+        mason_tool_installer.setup({
+            ensure_installed = {
+                "cssls",
+                "lua_ls",
+                "prettier", -- prettier formatter
+                "stylua", -- lua formatter
+                "isort", -- python formatter
+                "black", -- python formatter
+                "pylint",
+                "eslint_d",
+            },
+        })
+    end,
 }
