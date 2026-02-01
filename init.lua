16d6412a1a35c3ab3bc82c1b3885485acf019fca
Artem <154594695+Nighty3098@users.noreply.github.com>
2026-02-02 00:02:50 +0700

    Fix

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require("night.core")
 require("night.lazy")
 
 vim.api.nvim_set_option("clipboard", "unnamedplus")
-vim.cmd("colorscheme nightfly")
+vim.cmd("colorscheme ayu-mirage")
 vim.cmd("hi NvimTreeNormal guibg=nil")
 vim.cmd("hi NeoTreeNormal guibg=nil")
 vim.cmd("hi NeoTreeNormalNC guibg=nil")
