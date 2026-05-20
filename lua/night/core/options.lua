3a6d30cd8bfcabbbd84e1557bec0fc2e965ee61f
Nighty3098 <night3098games@gmail.com>
2026-05-21 02:11:02 +0700

    fix: replace deprecated vim.lsp.with and vim.loop APIs

--- lua/night/core/options.lua
+++ lua/night/core/options.lua
@@ -1,7 +1,15 @@
 local opt = vim.opt -- for conciseness
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+vim.lsp.handlers["textDocument/hover"] = function(err, result, ctx, config)
+  config = config or {}
+  config.border = "rounded"
+  vim.lsp.handlers.hover(err, result, ctx, config)
+end
+vim.lsp.handlers["textDocument/signatureHelp"] = function(err, result, ctx, config)
+  config = config or {}
+  config.border = "rounded"
+  vim.lsp.handlers.signature_help(err, result, ctx, config)
+end
 
 vim.opt.clipboard = "unnamedplus"
 
