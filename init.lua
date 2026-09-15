fc181bd7e139bc7a98c93c49daa7e202d419a1e9
Nighty3098 <night3098games@gmail.com>
2026-09-15 12:54:07 +0700

    style: switch default colorscheme to nightfly

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require("night.core")
 require("night.lazy")
 
 vim.api.nvim_set_option("clipboard", "unnamedplus")
-vim.cmd("colorscheme github_dark_high_contrast")
+vim.cmd("colorscheme nightfly")
 vim.cmd("hi NvimTreeNormal guibg=nil")
 vim.cmd("hi NeoTreeNormal guibg=nil")
 vim.cmd("hi NeoTreeNormalNC guibg=nil")
